3259f60cc27d2e81a5d016074816a039ddbdfcad
nielly_lima_123@hotmail.com <nielly_lima_123@hotmail.com>
2023-07-10 07:02:37 -0300

    add sessao Oferta Especial updated

--- fs13-loja-online-main/src/pages/Home/index.js
+++ fs13-loja-online-main/src/pages/Home/index.js
@@ -4,11 +4,8 @@ import Carousel from "../../components/Carousel";
 import TrendingProducts from "../../components/TrendingProducts";
 import Header from "../../components/Header";
 import { Menu } from "@mui/material";
-<<<<<<< HEAD
 import SpecialOffer from "../../components/SpecialOffer";
-=======
 import FeaturedCollections2 from "../../components/FeaturedCollections2";
->>>>>>> 8fe12e957f15fe9d75318512d99e23e022c0ddde
 
 export default function Home() {
     return (
